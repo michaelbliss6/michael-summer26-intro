c9f45ea3e47cd03f3b3ed6ae897ad6f387b3d5dd
Your Name <mtbliss@gmail.com>
2026-08-25 16:46:49 -0700

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -14,19 +14,60 @@
 
     document.body.appendChild(footer);
 
-    footer.style.margin = '20px'
+    footer.style.margin = '20px';
 
 
 /*SKILLS*/
 
-    const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"]
+    const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
     
     const skillsSection = document.getElementById('Skills');
 
-    const skillsList = skillsSection.querySelector('ul')
+    const skillsList = skillsSection.querySelector('ul');
 
     for (let i = 0; i < skills.length; i++) {
-        const skill = document.createElement('li')
+        const skill = document.createElement('li');
         skill.innerHTML = skills[i];
         skillsList.appendChild(skill);
     }
+
+/*MESSAGE FORM SUBMIT*/
+
+    function toggleMessagesVisibility() {
+        const messageSection = document.querySelector('#Messages');
+        
+
+        if (messageList.children.length === 0) {
+            messageSection.style.display = 'none';
+        } else {
+            messageSection.style.display = 'block';
+        }
+        }
+
+    
+    const messageForm = document.querySelector('form[name="leave_message"]');
+    const messageList = document.querySelector('#Messages ul');
+    
+    toggleMessagesVisibility();
+
+   messageForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const usersName = event.target.usersName.value;
+        const usersEmail = event.target.usersEmail.value;
+        const usersMessage = event.target.usersMessage.value;
+
+        console.log(usersName, usersEmail, usersMessage);
+        
+        const newMessage = document.createElement('li');
+        newMessage.innerHTML = `
+            <a href="mailto:${usersEmail}">${usersName}</a>
+            <span>${usersMessage}</span>
+            `;
+
+        messageList.appendChild(newMessage);
+
+        toggleMessagesVisibility();
+
+        event.target.reset();
+    });
